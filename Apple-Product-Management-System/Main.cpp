387b8b834683078a4a74a24b1a9443a08645f2c8
Shaunmak1214 <shaonmak@gmail.com>
2021-02-22 09:46:00 +0800

    feat: list implemented

--- Apple-Product-Management-System/Main.cpp
+++ Apple-Product-Management-System/Main.cpp
@@ -1,12 +1,86 @@
 #include "db.h"
 #include <mysql.h>
 #include <iostream>
+#include <string>
 
 using namespace std;
 int qstate;
 
+struct node {
+
+	int data;
+	node* next;
+
+};
+
+class ADTstack {
+
+private:
+    node* top;
+
+public:
+    ADTstack() : top(NULL) {}
+
+    int empty() {
+
+        if (top == NULL) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+
+    }
+
+    void push(int num) {
+
+        node* temp;
+        temp = new node;
+        temp->data = num;
+        if (!empty()) {
+            temp->next = top;
+            top = temp;
+        }
+        else {
+            top = temp;
+            top->next = NULL;
+        }
+
+    }
+
+    int pop() {
+
+        int num;
+        node* temp;
+
+        if (!empty()) {
+            temp = top;
+            num = top->data;
+            top = top->next;
+            delete temp;
+            return num;
+        }
+
+    }
+
+    void display() {
+
+        node* temp;
+        temp = top;
+
+        while (temp != NULL) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+
+    }
+};
+
+
 int main()
 {
+
+    ADTstack list;
 
 	MYSQL* conn;
 	MYSQL_ROW row;
@@ -26,6 +100,7 @@ int main()
 			while (row = mysql_fetch_row(res))
 			{
 				printf("ID: %s, GoogleId: %s, Name: %s\n", row[0], row[1], row[2]);
+                list.push(stoi(row[0]));
 			}
 		}
 		else
@@ -37,5 +112,7 @@ int main()
 	{
 		puts("Connection to database has failed!");
 	}
+
+    list.display();
 
 }
