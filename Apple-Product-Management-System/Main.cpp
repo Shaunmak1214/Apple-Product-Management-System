403557c37b3f1b45ab19e038ca1bdd079763c22d
Shaunmak1214 <shaonmak@gmail.com>
2021-02-26 05:35:11 +0800

    feat: linked list implemented

--- Apple-Product-Management-System/Main.cpp
+++ Apple-Product-Management-System/Main.cpp
@@ -6,86 +6,81 @@
 using namespace std;
 int qstate;
 
-struct node {
+struct Node {
 
-	int data;
-	node* next;
+	string data;
+	Node* next;
 
 };
 
-class ADTstack {
+Node* head;
 
-private:
-    node* top;
+class linkedList {
 
-public:
-    ADTstack() : top(NULL) {}
+	public:
+		int data;
 
-    int empty() {
+	void append(Node** head_ref, string new_data)
+	{
+		/* 1. allocate node */
+		Node* new_node = new Node();
 
-        if (top == NULL) {
-            return 1;
-        }
-        else {
-            return 0;
-        }
+		Node* last = *head_ref; /* used in step 5*/
 
-    }
+		/* 2. put in the data */
+		new_node->data = new_data;
 
-    void push(int num) {
+		/* 3. This new node is going to be
+		the last node, so make next of
+		it as NULL*/
+		new_node->next = NULL;
 
-        node* temp;
-        temp = new node;
-        temp->data = num;
-        if (!empty()) {
-            temp->next = top;
-            top = temp;
-        }
-        else {
-            top = temp;
-            top->next = NULL;
-        }
+		/* 4. If the Linked List is empty,
+		then make the new node as head */
+		if (*head_ref == NULL)
+		{
+			*head_ref = new_node;
+			return;
+		}
 
-    }
+		/* 5. Else traverse till the last node */
+		while (last->next != NULL)
+			last = last->next;
 
-    int pop() {
+		/* 6. Change the next of last node */
+		last->next = new_node;
+		return;
+	}
 
-        int num;
-        node* temp;
+	void printList(Node* node)
+	{
+		int counter = 1;
 
-        if (!empty()) {
-            temp = top;
-            num = top->data;
-            top = top->next;
-            delete temp;
-            return num;
-        }
+		cout << endl;
+		cout << "=============" << endl;
+		cout << " Linked List" << endl;
+		cout << "=============" << endl;
 
-    }
+		while (node != NULL)
+		{
+			cout << "Index " << counter << ":" << "  " << node->data << endl;
+			node = node->next;
 
-    void display() {
+			counter++;
+		}
+	}
 
-        node* temp;
-        temp = top;
-
-        while (temp != NULL) {
-            cout << temp->data << " ";
-            temp = temp->next;
-        }
-
-    }
 };
-
 
 int main()
 {
 
-    ADTstack list;
-
+	head = NULL;
 	MYSQL* conn;
 	MYSQL_ROW row;
 	MYSQL_RES* res;
 	conn = initConnection();
+	linkedList list;
 
 	if (conn) 
 	{
@@ -100,7 +95,9 @@ int main()
 			while (row = mysql_fetch_row(res))
 			{
 				printf("ID: %s, GoogleId: %s, Name: %s\n", row[0], row[1], row[2]);
-                list.push(stoi(row[0]));
+                //list.push(stoi(row[0]));
+				string id = row[2];
+				list.append(&head, id);
 			}
 		}
 		else
@@ -113,6 +110,7 @@ int main()
 		puts("Connection to database has failed!");
 	}
 
-    list.display();
+
+	list.printList(head);
 
 }
