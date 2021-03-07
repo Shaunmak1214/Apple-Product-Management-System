01ac3178acbdf77400523b6a705a701c6c2c6449
Yeeshuen Too <66751584+YeeshuenToo@users.noreply.github.com>
2021-03-07 17:25:18 +0800

    update Hashing

--- Apple-Product-Management-System/Hashing.h
+++ Apple-Product-Management-System/Hashing.h
@@ -21,13 +21,13 @@ void searchCode()
 	cout << "+---------------------------------------------------+" << endl;
 	cout << "                SEARCH BY PRODUCT CODE               " << endl;
 	cout << "+---------------------------------------------------+" << endl;
-	cout << "	Product Category						Code	  " << endl;
+	cout << "	Product Category		Code	  "					<< endl;
 	cout << "\t 1. Accessories		-------------------  101	  " << endl;
-	cout << "\t 2. Laptop			-------------------  202	  " << endl;
+	cout << "\t 2. Laptop		-------------------  202	  "		<< endl;
 	cout << "\t 3. Accessories		-------------------  303	  " << endl;
 	cout << "\t 4. Accessories		-------------------  404	  " << endl;
 	cout << "\t 5. Accessories		-------------------  505	  " << endl;
-	cout << "\t 6. Others			-------------------  909	  " << endl;
+	cout << "\t 6. Others		-------------------  909	  "		<< endl;
 	cout << "+---------------------------------------------------+" << endl << endl;
 	cout << "Enter the product code: ";
 	cin >> hashedCode;
@@ -45,21 +45,22 @@ void searchCode()
 			int numfields = mysql_num_fields(res);
 			int i = 0;
 
+			//cout << row[2] << endl;
+			cout << "Product Hashed by Key Entered : " << endl;
+			cout << setw(4) << left << "No" << setw(15) << "Id" << setw(30) << "Name" << setw(15) << "Category" << setw(10) << "Price" << setw(25) << "Colors" << endl;
+
 			while (row = mysql_fetch_row(res))
 			{
 				int comparedHash = hashing(stoi(row[1]));
 				if (comparedHash == hashedCode) {
 
-					//cout << row[2] << endl;
-					cout << "Product Hashed by Key Entered : " << endl;
-					cout << setw(5) << left << "No" << setw(5) << "Id" << setw(8) << "Code" << setw(30) << "Name" << setw(15) << "Category" << setw(10) << "Price" << setw(25) << "Colors" << endl;
-					cout << setw(5) << i + 1 << setw(5) << row[1] << setw(8) << row[2] << setw(30) << row[3] << setw(15) << row[4] << setw(10) << row[5] << setw(25) << row[6] << endl;
+					cout << setw(4) << (i+1) << setw(15) << row[1] << setw(30) << row[2] << setw(15) << row[3] << setw(10) << row[4] << setw(25) << row[5] << endl;
 
 				}
-
 				i++;
-
 			}
+			
+			cout << endl;
 
 		}
 		else
